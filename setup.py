92dccbc6d316dc3fc2b3a2e601ff6d5f5255c323
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2025-03-10 19:23:57 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='ihack',
-    version='3.0.6',
+    version='3.0.7',
     author='Termux Hackers ID (@iqbalmh18)',
     author_email='termuxhackers.id@gmail.com',
-    description='A python based tool for hacking instagram accounts',
-    long_description='InstaHack: A python-based tool for hacking instagram accounts using brute force and dictionary attacks with private api from latest apk pinning',
+    description='Python based tools for hacking instagram accounts',
+    long_description='InstaHack is a python based tools for hacking instagram accounts using brute force and dictionary attacks with private api from latest apk pinning',
     url='https://github.com/termuxhackers-id/instahack',
     packages=find_packages(),
     include_package_data=True,
     package_data={'instahack': ['ihack']},
-    install_requires=['rich','beaupy','glickey','ihackpy','requests','prompt_toolkit'],
+    install_requires=['rich','beaupy','glickey','ihackit','requests','prompt_toolkit'],
     entry_points={
         'console_scripts': [
             'ihack = instahack.main:main',
